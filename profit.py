a633568e1c89281929ba99f315d18c6bd3382986
Akshay-ingle <akshy.nilesh.ingle@gmail.com>
2024-11-08 19:46:27 +0530

    profi

---
+++ profit.py
@@ -0,0 +1,10 @@
+profit=float(input('enter your actual amout:'))
+
+amount=float(input('enter your slaing amount:'))
+
+if (amount>profit):
+     loop=amount-profit 
+     print('total profit={0}'.format (loop))
+
+else:
+      print('no profit')
